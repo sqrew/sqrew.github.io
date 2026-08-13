07fff294c29373ea30d05f6f3bce8270755aee40
sqrew <nicholaspmanson@gmail.com>
2026-08-13 19:29:33 -0400

    Fix auto-derived bind group mismatch by removing unused sampler

--- nii-viewer/index.js
+++ nii-viewer/index.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp4fd9d8mr.js
+// include: /tmp/tmpwr57z3iq.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -203,21 +203,21 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmp4fd9d8mr.js
-// include: /tmp/tmpqqr_iq9t.js
+// end include: /tmp/tmpwr57z3iq.js
+// include: /tmp/tmpnswduh62.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpqqr_iq9t.js
-// include: /tmp/tmptefamb4k.js
+  // end include: /tmp/tmpnswduh62.js
+// include: /tmp/tmpjx3qdn6y.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmptefamb4k.js
+  // end include: /tmp/tmpjx3qdn6y.js
 
 
 var programArgs = [];
