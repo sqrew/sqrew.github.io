a61fa591bd6b21b6457f6f03f035fb8ac381c943
sqrew <nicholaspmanson@gmail.com>
2026-08-13 19:38:57 -0400

    Implement dynamic density threshold adjustments with Z and X keys

--- nii-viewer/index.js
+++ nii-viewer/index.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpzybmkq4c.js
+// include: /tmp/tmpyt4txmkm.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -203,21 +203,21 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmpzybmkq4c.js
-// include: /tmp/tmplhnhpst5.js
+// end include: /tmp/tmpyt4txmkm.js
+// include: /tmp/tmp3csrdhop.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmplhnhpst5.js
-// include: /tmp/tmpun3wf9a_.js
+  // end include: /tmp/tmp3csrdhop.js
+// include: /tmp/tmpb2xza2d4.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpun3wf9a_.js
+  // end include: /tmp/tmpb2xza2d4.js
 
 
 var programArgs = [];
