b3dc5328ac2f78db46ba5260763e955593e04553
sqrew <nicholaspmanson@gmail.com>
2026-08-13 20:47:18 -0400

    Fix file input dialog block by using native JS keydown listener

--- nii-viewer/index.js
+++ nii-viewer/index.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpd8h1gxl0.js
+// include: /tmp/tmpvaoybtt9.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -203,21 +203,21 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmpd8h1gxl0.js
-// include: /tmp/tmpiz87rlqs.js
+// end include: /tmp/tmpvaoybtt9.js
+// include: /tmp/tmppy50vg3r.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpiz87rlqs.js
-// include: /tmp/tmpm_7la6hg.js
+  // end include: /tmp/tmppy50vg3r.js
+// include: /tmp/tmp0u74qfc3.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpm_7la6hg.js
+  // end include: /tmp/tmp0u74qfc3.js
 
 
 var programArgs = [];
