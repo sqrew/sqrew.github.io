779acf3c778ceab07b3971720a9d907cf5a4d679
sqrew <nicholaspmanson@gmail.com>
2026-08-16 10:30:08 -0400

    Capture pointer lock directly upon requesting native fullscreen

--- nii-viewer/index.js
+++ nii-viewer/index.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp9r58sv1m.js
+// include: /tmp/tmpwmfhfh0i.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -203,21 +203,21 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmp9r58sv1m.js
-// include: /tmp/tmpp5jpyjqn.js
+// end include: /tmp/tmpwmfhfh0i.js
+// include: /tmp/tmprcor7lab.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpp5jpyjqn.js
-// include: /tmp/tmp8cbv2j9x.js
+  // end include: /tmp/tmprcor7lab.js
+// include: /tmp/tmp703mlqlq.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp8cbv2j9x.js
+  // end include: /tmp/tmp703mlqlq.js
 
 
 var programArgs = [];
