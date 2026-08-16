0258e2f775fc242fbf7a26df84a1690a8caedbf3
sqrew <nicholaspmanson@gmail.com>
2026-08-16 11:09:12 -0400

    Implement dual threshold band-pass density segmenting

--- nii-viewer/index.js
+++ nii-viewer/index.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp9rrcvea9.js
+// include: /tmp/tmp6m21rkrk.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -203,21 +203,21 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmp9rrcvea9.js
-// include: /tmp/tmpljdlftgh.js
+// end include: /tmp/tmp6m21rkrk.js
+// include: /tmp/tmpttfkq80p.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpljdlftgh.js
-// include: /tmp/tmpl5rofc9q.js
+  // end include: /tmp/tmpttfkq80p.js
+// include: /tmp/tmpfd4bubv7.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpl5rofc9q.js
+  // end include: /tmp/tmpfd4bubv7.js
 
 
 var programArgs = [];
@@ -9105,8 +9105,9 @@ function checkIncomingModuleAPI() {
   ignoredModuleProp('wasmBinary');
 }
 var ASM_CONSTS = {
-  88500: ($0, $1) => { if (window.updateClippingSlider) { window.updateClippingSlider($0, $1); } },  
- 88578: () => { if (window.triggerNiftiFileSelect) { window.triggerNiftiFileSelect(); } }
+  88756: ($0) => { if (window.updateMinThresholdSlider) { window.updateMinThresholdSlider($0); } },  
+ 88838: ($0, $1) => { if (window.updateClippingSlider) { window.updateClippingSlider($0, $1); } },  
+ 88916: () => { if (window.triggerNiftiFileSelect) { window.triggerNiftiFileSelect(); } }
 };
 
 // Imports from the Wasm binary.
@@ -9114,6 +9115,8 @@ var _malloc = Module['_malloc'] = makeInvalidEarlyAccess('_malloc');
 var _free = Module['_free'] = makeInvalidEarlyAccess('_free');
 var _nii_load_file_data = Module['_nii_load_file_data'] = makeInvalidEarlyAccess('_nii_load_file_data');
 var _nii_set_clipping = Module['_nii_set_clipping'] = makeInvalidEarlyAccess('_nii_set_clipping');
+var _nii_set_min_threshold = Module['_nii_set_min_threshold'] = makeInvalidEarlyAccess('_nii_set_min_threshold');
+var _nii_set_max_threshold = Module['_nii_set_max_threshold'] = makeInvalidEarlyAccess('_nii_set_max_threshold');
 var _fflush = makeInvalidEarlyAccess('_fflush');
 var _strerror = makeInvalidEarlyAccess('_strerror');
 var _main = Module['_main'] = makeInvalidEarlyAccess('_main');
@@ -9183,6 +9186,8 @@ function assignWasmExports(wasmExports) {
   assert(typeof wasmExports['free'] != 'undefined', 'missing Wasm export: free');
   assert(typeof wasmExports['nii_load_file_data'] != 'undefined', 'missing Wasm export: nii_load_file_data');
   assert(typeof wasmExports['nii_set_clipping'] != 'undefined', 'missing Wasm export: nii_set_clipping');
+  assert(typeof wasmExports['nii_set_min_threshold'] != 'undefined', 'missing Wasm export: nii_set_min_threshold');
+  assert(typeof wasmExports['nii_set_max_threshold'] != 'undefined', 'missing Wasm export: nii_set_max_threshold');
   assert(typeof wasmExports['fflush'] != 'undefined', 'missing Wasm export: fflush');
   assert(typeof wasmExports['strerror'] != 'undefined', 'missing Wasm export: strerror');
   assert(typeof wasmExports['__main_argc_argv'] != 'undefined', 'missing Wasm export: __main_argc_argv');
@@ -9249,6 +9254,8 @@ function assignWasmExports(wasmExports) {
   _free = Module['_free'] = createExportWrapper('free', wasmExports['free'], 1);
   _nii_load_file_data = Module['_nii_load_file_data'] = createExportWrapper('nii_load_file_data', wasmExports['nii_load_file_data'], 2);
   _nii_set_clipping = Module['_nii_set_clipping'] = createExportWrapper('nii_set_clipping', wasmExports['nii_set_clipping'], 2);
+  _nii_set_min_threshold = Module['_nii_set_min_threshold'] = createExportWrapper('nii_set_min_threshold', wasmExports['nii_set_min_threshold'], 1);
+  _nii_set_max_threshold = Module['_nii_set_max_threshold'] = createExportWrapper('nii_set_max_threshold', wasmExports['nii_set_max_threshold'], 1);
   _fflush = createExportWrapper('fflush', wasmExports['fflush'], 1);
   _strerror = createExportWrapper('strerror', wasmExports['strerror'], 1);
   _main = Module['_main'] = createExportWrapper('__main_argc_argv', wasmExports['__main_argc_argv'], 2);
